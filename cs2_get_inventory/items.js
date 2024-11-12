4d3f7a61bdcbaaa2b5dc6efd77ee2a7251792718
Paweł Teodorczyk <74329914+paweteq@users.noreply.github.com>
2024-11-12 23:31:04 +0100

    Add files via upload

--- cs2_get_inventory/items.js
+++ cs2_get_inventory/items.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 const VDF = require('@node-steam/vdf');
+const casehardened = require('./casehardened.js')
+const {
+    FadeCalculator,
+    AmberFadeCalculator,
+    AcidFadeCalculator,
+  } = require('csgo-fade-percentage-calculator');
 function parse_data(path) {
     try {
         const data = fs.readFileSync(path, 'utf8');
@@ -8,6 +14,40 @@ function parse_data(path) {
     } catch (err) {
         console.error(err);                          
     }
+}
+function get_weapon_name(item){
+    let prefab_name = "";
+     if (items?.['items_game']?.['items']?.[item.def_index]?.['item_name']) {
+        prefab_name = items['items_game']['items'][item.def_index]['item_name'];
+    } else {
+        let prefab = items['items_game']['items'][item.def_index]['prefab'];
+        prefab_name = items['items_game']['prefabs'][prefab].item_name
+        
+    }
+     prefab_name = prefab_name.replace('#','')
+     const Weapon_name = translation['lang']['Tokens'][prefab_name]
+     return Weapon_name
+}
+function get_name_sticker(item){
+    console.log(item)
+    let list_sticker = []
+    item['stickers'].forEach(sticker => {
+        let sticker_id = sticker.sticker_id
+        let sticker_name = items['items_game']['sticker_kits'][sticker_id].item_name
+        sticker_name = sticker_name.replace('#','')
+        let res = translation['lang']['Tokens'][sticker_name]
+        const wear_sticker = sticker.wear
+        let sticker_wear = 1.00
+        if (wear_sticker != null){
+            sticker_wear = wear_sticker.toFixed(2)
+        }
+        const json = {
+            name: res,
+            wear: sticker_wear,
+        }
+        list_sticker.push(json)
+    });
+    return list_sticker
 }
 
 const items = parse_data('./items_game.txt');
@@ -54,6 +94,42 @@ async function Process_Items(items, csgo){
             item_res['def_index'] = item.def_index
             item_res['rarity'] = item.rarity
             item_res['name'] = get_name(item , item_type)
+            if (item['stickers'] && item_type == "weapon_skin"){
+                if (item['stickers'].length > 0){
+                item_res['stickers'] = get_name_sticker(item)
+            }}
+            let state_of_dopler = null
+            if (item_res['name'].includes('Doppler') || item_res['name'].includes('Gamma Doppler')){
+                state_of_dopler = items['items_game']['paint_kits'][item.paint_index].name
+            }
+            const dopplers = {
+                    "am_doppler_phase1": "Phase 1",
+                    "am_doppler_phase2": "Phase 2",
+                    "am_doppler_phase3": "Phase 3",
+                    "am_doppler_phase4": "Phase 4",
+
+                    "am_gamma_doppler_phase1": "Phase 1",
+                    "am_gamma_doppler_phase2": "Phase 2",
+                    "am_gamma_doppler_phase3": "Phase 3",
+                    "am_gamma_doppler_phase4": "Phase 4",
+
+                    "am_blackpearl_marbleized_b": "Black Pearl",
+                    "am_sapphire_marbleized_b": "Sapphire",
+                    "am_ruby_marbleized": "Ruby",
+                    "am_emerald_marbleized": "Emerald",
+                    
+                }
+                if (state_of_dopler != null){
+                    for (const key in dopplers) {
+                        if (state_of_dopler && key.includes(state_of_dopler)) {
+                            item_res['info_seed'] = dopplers[key];
+                            break;
+                        }
+                    }
+                }
+            if (item_res['name'].includes('Case Hardened')){
+                
+            }
             if (item_type == "coin" || item_res['name'] == "Music KIT | Valve"){
                 item_res['tradeable'] = false
             }
@@ -91,6 +167,33 @@ async function Process_Items(items, csgo){
                 let data = keychain_seed.readInt32LE(0);
                 item_res['keychain_seed'] = data
             }
+            if (item_type == "keychain"){
+                const info_seed = analyze_seed_of_keychain(item_res['keychain_seed'], item_res['name'])
+                if (info_seed != null){
+                    item_res['info_seed'] = info_seed
+                }
+            }
+            if (item_res['name'].includes("Case Hardened")){
+                const gem = casehardened.Check_Gem(item_res['name'], item.paint_seed)
+                if (gem != null){
+                    item_res['info_seed'] = gem
+                }
+            }
+            if (item_res['name'].includes("Acid") || item_res['name'].includes("Amber") || item_res['name'].includes("Fade")){
+                const weapon_name = get_weapon_name(item)
+                if (item_res['name'].includes("Acid")){
+                    const Fade_Percentage = AcidFadeCalculator.getFadePercentage(weapon_name, item.paint_seed)
+                    item_res['fade_percentage'] = Fade_Percentage
+                }
+                else if (item_res['name'].includes("Amber")){
+                    const Fade_Percentage = AmberFadeCalculator.getFadePercentage(weapon_name, item.paint_seed)
+                    item_res['fade_percentage'] = Fade_Percentage
+                }
+                else if (item_res['name'].includes("Fade")){
+                    const Fade_Percentage = FadeCalculator.getFadePercentage(weapon_name, item.paint_seed)
+                    item_res['fade_percentage'] = Fade_Percentage
+                }
+            }
             item_res['type'] = item_type
             res.push(item_res)
         }
@@ -98,6 +201,192 @@ async function Process_Items(items, csgo){
            
     }
     return res
+
+}
+function analyze_seed_of_keychain(seed, name){
+    if (name.includes('Semi-Precious')){
+        if (seed == 11285){
+            return "Emerald"
+        }
+        else if (seed > 1 && seed <= 10000){
+            return "Light Green"
+        }
+        else if (seed > 10000 && seed <= 20000){
+            return "Dark Green"
+        }
+        else if (seed > 20000 && seed <= 30000){
+            return "Teal"
+        }
+        else if (seed > 30000 && seed <= 50000){
+            return "Cyan"
+        }
+        else if (seed > 50000 && seed <= 60000){
+            return "Light Blue"
+        }
+        else if (seed > 60000 && seed <= 80000){
+            return "Mix Blue / Purple"
+        }
+        else if (seed > 80000 && seed <= 90000){
+            return "Mix Purple / Blue"
+        }
+        else if (seed > 90000 && seed <= 100000){
+            return "Purple"
+        }
+    }
+    if (name.includes('Baby Karat CT')){
+        if (seed > 1 && seed <= 50000){
+            return "Bronze 1"
+        }
+        else{
+            return "More Gold"
+        }
+    }
+    if (name.includes('Baby Karat T')){
+        if (seed > 1 && seed <= 50000){
+            return "Bronze 1"
+        }
+        else{
+            return "More Gold"
+        }
+    }
+    if (name.includes('Die-cast AK')){
+       if (seed > 1 && seed <= 10000){
+            return 'Gold'
+        }
+        else if (seed > 10000 && seed <= 20000){
+            return "Orange"
+        }
+        else if (seed > 20000 && seed <= 25000){
+            return "Red"
+        }
+        else if (seed > 25000 && seed <= 60000){
+            return "Pink"
+        }
+        else if (seed > 60000 && seed <= 87000){
+            return "Purple"
+        }
+        else if (seed > 87000 && seed <= 98000){
+            return "Blue"
+        }
+        else if (seed > 98000 && seed <= 100000){
+            return "Light Blue"
+        }
+    }
+    if (name.includes('Titeenium AWP')){
+        if (seed > 1 && seed <= 20000){
+            return 'Yellow'
+        }
+        else if (seed > 20000 && seed <= 40000){
+            return "Green"
+        }
+        else if (seed > 40000 && seed <= 60000){
+            return "Cyan"
+        }
+        else if (seed > 60000 && seed <= 80000){
+            return "Blue"
+        }
+        else if (seed > 80000 && seed <= 100000){
+            return "Purple/Pink"
+        }
+    }
+    if (name.includes("Lil' Teacup")){
+        if (seed == 99606){
+            return "Smoke"
+        }
+    }
+    if (name.includes("Hot Wurst")){
+        if (seed == 87807){
+            return "Liquid fire"
+        }
+        else if (seed == 7632){
+            return "Liquid fire"
+        }
+        else if (seed > 1 && seed <= 15000){
+            return "Worse HOLO Effect"
+        }
+        else{
+            return "Better HOLO Effect"
+        }
+    }
+    if (name.includes("Chicken Lil'")){
+        if (seed == 307){
+            return "Blue Gem"
+        }
+    }
+    if (name.includes("Hot Howl")){
+        if (seed > 1 && seed <= 15000){
+            return "More Reddish"
+        }
+        else{
+            return "More yellowish"
+        }
+    }
+    if (name.includes("Lil' Monster")){
+        if (seed == 38){
+            return "Red Tongue"
+        }
+        else if (seed == 99205){
+            return "Blue / Purple Tongue"
+        }
+    }
+    if (name.includes("Diamond Dog")){
+            if (seed > 1 && seed <= 50000){
+                return "More Pealescent"
+            }
+            else if (seed > 50000 && seed <= 100000){
+                return "More White"
+            }
+    if (name.includes("That's Bananas")){
+        if (seed > 1 && seed <= 5000){
+            return "Bronze"
+        }
+        else if (seed > 5000 && seed <= 20000){
+            return "Dark Yellow"
+        }
+        else if (seed > 20000 && seed <= 98000){
+            return "Yellow"
+        }
+        else if (seed > 98000 && seed <= 100000){
+            return "Lime Green"
+        }
+    }
+    if (name.includes("Lil' SAS")){
+        if (seed == 432){
+            return "Red"
+        }
+    }
+    if (name.includes("Hot Sauce")){
+        if (seed == 741){
+            return "Coca Cola"
+        }
+        else if (seed == 99995){
+            return "Pink eye"
+        }
+    }
+    if (name.includes("Lil' Crass")){
+        if (seed > 1 && seed <= 25000){
+            return "Red"
+        }
+        else if (seed > 25000 && seed <= 40000){
+            return "Orange"
+        }
+        else if (seed > 40000 && seed <= 75000){
+            return "Yellow"
+        }
+        else if (seed > 75000 && seed <= 84000){
+            return "Lime Green"
+        }
+        else if (seed > 84000 && seed <= 93000){
+            return "Green"
+        }
+        else if (seed > 93000 && seed <= 97000){
+            return "Teal"
+        }
+        else if (seed > 97000 && seed <= 100000){
+            return "Cyan"
+        }
+    }
+    }
 
 }
 function analayze_type_of_skin(item){
